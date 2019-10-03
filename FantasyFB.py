d5e7d0edb2a15fc69aec9d1d4b6a3ffd039c5d4a
Donsmith4090 <54415433+Donsmith4090@users.noreply.github.com>
2019-10-02 21:33:01 -0400

    Update FantasyFB.py

--- FantasyFB.py
+++ FantasyFB.py
@@ -16,3 +16,23 @@ int4 = int(pointsOnTds)
 totalPoints = (int3+int4)
 int5 = int(totalPoints)
 print(name , pos , "total points were", int5)
+
+
+print('hello welcome to the fantasy football calculator!')
+print('Print 1 for WR, 2 for RB, 3 for TE, 4 for QB')
+pos = input('What position is the player?')
+name = input('What is the name of the player?')
+weekRushTds = int(input('How many touchdowns did your player rush or catch this week?'))
+weekReceivingYds = int(input('How many yards did your player have receiving or rushing this week?'))
+receps = int(input('How many receptions did your player have this week?'))
+weekRushTds = float(weekRushTds)
+weekReceivingYards = float(weekReceivingYds)
+receps = float(receps)
+weekPassTds = int(input('How many touchdowns did your player throw for?'))
+weekPassYds = int(input('How many yards did your player throw for?'))
+if pos ==  1 or 2 or 3:
+    totalPoints = int(((weekRushTds * 6) + (weekReceivingYds *  .1) + (weekPassTds * 4) + (weekPassYds * .05) + (receps)))
+    print('Total points were',format(totalPoints,'.2f'))
+elif pos == 4:
+    totalPoints = int(((weekPassTds * 4) + (weekPassYds * .05) + (weekRushTds * 6) + (weekReceivingYds *  .1) + (receps)))
+    print('Total Points are',(totalPoints,'.2f'))
